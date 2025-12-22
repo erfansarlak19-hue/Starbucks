f2df5223faed5f1218df9502121207e8d1bec3a6
erfansarlak19-hue <erfansarlak19@gmail.com>
2025-12-22 15:19:37 +0330

    feat: finish design

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,6 +1,7 @@
-export default function Cards() {
+export default function Cart() {
   return (
-		<div>
+		<div className="flex flex-col gap-3">
+      <p className="text-center text-xl font-semibold">Bill</p>
 			<div className="flex ml-3 gap-3">
 				<div className="bg-[#1e3932] flex flex-col w-fit p-3 rounded-xl gap-3">
 					<img
@@ -10,13 +11,7 @@ export default function Cards() {
 					/>
 					<h1 className="text-white font-semibold text-">Cappuccino</h1>
 					<p className="text-[#f8d1ad] font-bold">$3.5</p>
-					<div className="flex justify-center">
-						<button className="bg-[#f8d1ad] w-8 h-8 cursor-pointer">+</button>
-						<p className="bg-white w-8 h-8 flex items-center justify-center ">
-							0
-						</p>
-						<button className="bg-[#f8d1ad] w-8 h-8 cursor-pointer">-</button>
-					</div>
+					<p className="text-white">Qty: 0</p>
 				</div>
 			</div>
 		</div>
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,17 +1,20 @@
 import Cards from "../cards/Cards";
+import Cart from "../cart/Cart";
 
 export default function Home() {
   return (
-		<div className="bg-[#ececec] flex flex-col">
-			<div className="flex flex-col justify-items-center items-center pb-3">
+		<div className="bg-[#ececec] flex flex-col gap-3 pt-3 justify-center items-center pl-3 pr-3">
+			<div className="flex flex-col gap-2 ">
 				<div className="flex items-center gap-3 ">
 					<img src="./public/logo/logo.png" alt="logo" className="w-17" />
 					<h1 className="text-4xl font-bold">Starbucks</h1>
 				</div>
 				<h2 className="text-xl font-semibold">Starbucks Online Coffee Order</h2>
 			</div>
-      <Cards/>
-      <div></div>
+			<Cards />
+			<Cart />
+			<p className="text-xl font-semibold">Total:$0</p>
+			<button className="bg-[#e9c9a2] w-full rounded-sm text-white font-semibold p-2 cursor-pointer">Submit Order</button>
 		</div>
 	);
 }
