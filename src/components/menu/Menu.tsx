d96fc50267bdb4413f0aedfa80b070650d41f1aa
erfansarlak19-hue <erfansarlak19@gmail.com>
2025-12-22 13:45:42 +0330

    start project

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+		<div>
+			<div className="flex flex-col justify-items-center items-center">
+				<div className="flex ">
+          <img src="./public/logo/logo.png" alt="logo" className="w-17" />
+					<h1 className="text-4xl font-bold">Starbucks</h1>
+				</div>
+				<h2 className="text-xl font-semibold">Starbucks Online Coffee Order</h2>
+			</div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+}
---
+++ src/pages/menuPage/MenuPage.tsx
@@ -0,0 +1,9 @@
+import Home from "../../components/menu/Menu";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
