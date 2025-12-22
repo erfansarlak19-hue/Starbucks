91b44ef3db45607ebf24be7ecd38e1033ed09691
erfansarlak19-hue <erfansarlak19@gmail.com>
2025-12-22 14:44:27 +0330

    feat: Add card design

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,17 @@
+import Cards from "../cards/Cards";
+
 export default function Home() {
   return (
-		<div>
-			<div className="flex flex-col justify-items-center items-center">
-				<div className="flex ">
-          <img src="./public/logo/logo.png" alt="logo" className="w-17" />
+		<div className="bg-[#ececec] flex flex-col">
+			<div className="flex flex-col justify-items-center items-center pb-3">
+				<div className="flex items-center gap-3 ">
+					<img src="./public/logo/logo.png" alt="logo" className="w-17" />
 					<h1 className="text-4xl font-bold">Starbucks</h1>
 				</div>
 				<h2 className="text-xl font-semibold">Starbucks Online Coffee Order</h2>
 			</div>
-			<div></div>
-			<div></div>
+      <Cards/>
+      <div></div>
 		</div>
 	);
 }
